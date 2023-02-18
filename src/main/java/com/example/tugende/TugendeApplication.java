a3c8b760dba325f0576d596489dc4491f50c4740
“ronaldssebalamu” <ronaldssebalamu2@gmail.com>
2023-02-18 10:58:08 +0300

    Powering the application

---
+++ src/main/java/com/example/tugende/TugendeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tugende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugendeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugendeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tugende/TugendeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tugende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugendeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
